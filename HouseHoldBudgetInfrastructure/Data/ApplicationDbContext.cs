6328f1dc79325891f4b488212c404e06e4bc0e16
Tancheto <tanya.zineva@gmail.com>
2022-11-27 17:45:15 +0200

    Creating a projects

--- HouseHoldBudgetInfrastructure/Data/ApplicationDbContext.cs
+++ HouseHoldBudgetInfrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HouseHoldBudget.Data
+namespace HouseHoldBudget.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
